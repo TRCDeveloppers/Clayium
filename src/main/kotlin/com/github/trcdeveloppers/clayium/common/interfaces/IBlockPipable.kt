072cc4d06cf335a856caf64fcef6d9ae67553517
bqc0n <b4c0nchan@gmail.com>
2024-01-08 23:18:50 +0900

    add IProperty isPipe to IBlockPipable

--- src/main/kotlin/com/github/trcdeveloppers/clayium/common/interfaces/IBlockPipable.kt
+++ src/main/kotlin/com/github/trcdeveloppers/clayium/common/interfaces/IBlockPipable.kt
@@ -1,5 +1,12 @@
 package com.github.trcdeveloppers.clayium.common.interfaces
 
+import net.minecraft.block.properties.IProperty
+import net.minecraft.block.properties.PropertyBool
+
 interface IBlockPipable {
     var isPipe: Boolean
+
+    companion object {
+        val isPipe: IProperty<Boolean> = PropertyBool.create("is_pipe")
+    }
 }
